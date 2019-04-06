e7bec45566b9ca2ccd95ae74f14f0fb506e73db0
Oleh Poliukhovych <aleguk@ukr.net>
2019-04-06 22:31:58 +0300

    create metod

---
+++ create_metod.rb
@@ -0,0 +1,15 @@
+def accelerate
+	puts "Stepping on the gas"
+	puts "Speeding up"
+end
+def sound_horn
+	puts "Pressing the horn button"
+	puts "Beep beep"
+end
+def use_headlights (brightness)
+	puts "Turning on #{brightness} headlights"
+	puts "Watch out for deep!"
+end
+sound_horn
+accelerate
+use_headlights("high-beam")

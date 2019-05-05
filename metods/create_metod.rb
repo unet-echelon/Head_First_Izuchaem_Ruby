9529647caba3c9dfb5cb5ab99caee13be63299f4
Oleh Poliukhovych <aleguk@ukr.net>
2019-05-05 16:03:40 +0300

    sozdanie metoda,vyzov, parametru

--- metods/create_metod.rb
+++ metods/create_metod.rb
@@ -6,10 +6,12 @@ def sound_horn
 	puts "Pressing the horn button"
 	puts "Beep beep"
 end
-def use_headlights (brightness)
+def use_headlights (brightness = "low-beam")
 	puts "Turning on #{brightness} headlights"
 	puts "Watch out for deep!"
 end
 sound_horn
+sleep 2
 accelerate
-use_headlights("high-beam")
+sleep 2
+use_headlights
